24a0b292db76644b360b8fcf8234e524bd9132ac
Pranay <pranaysahith7@gmail.com>
2021-01-27 18:15:11 +0000

    create separate file

---
+++ packer/target/icap-server/install-wizard.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# install wizard for ova/esxi.
+# defining vars
+DEBIAN_FRONTEND=noninteractive
+KERNEL_BOOT_LINE='net.ifnames=0 biosdevname=0'
+
+# sudo without password prompt
+echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER >/dev/null
+
+# update packages
+sudo apt update && sudo apt upgrade -y
+
+# install needed packages
+sudo apt install -y telnet tcpdump open-vm-tools net-tools dialog curl git sed grep fail2ban
+sudo systemctl enable fail2ban.service
+sudo tee -a /etc/fail2ban/jail.d/sshd.conf << EOF > /dev/null
+[sshd]
+enabled = true
+port = ssh
+action = iptables-multiport
+logpath = /var/log/auth.log
+bantime  = 10h
+findtime = 10m
+maxretry = 5
+EOF
+sudo systemctl restart fail2ban
+
+# switching to predictable network interfaces naming
+grep "$KERNEL_BOOT_LINE" /etc/default/grub >/dev/null || sudo sed -Ei "s/GRUB_CMDLINE_LINUX=\"(.*)\"/GRUB_CMDLINE_LINUX=\"\1 $KERNEL_BOOT_LINE\"/g" /etc/default/grub
+
+# cloning vmware scripts repo
+git clone --single-branch -b main https://github.com/k8-proxy/vmware-scripts.git ~/scripts
+
+# installing the wizard
+sudo install -T ~/scripts/scripts/wizard/wizard.sh /usr/local/bin/wizard -m 0755
+
+# installing initconfig ( for running wizard on reboot )
+sudo cp -f ~/scripts/scripts/bootscript/initconfig.service /etc/systemd/system/initconfig.service
+sudo install -T ~/scripts/scripts/bootscript/initconfig.sh /usr/local/bin/initconfig.sh -m 0755
+sudo systemctl daemon-reload
+
+# enable initconfig for the next reboot
+sudo systemctl enable initconfig
