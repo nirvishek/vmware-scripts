08231fdad7986ad202a295cf2626b9328921496a
Pranay <pranaysahith7@gmail.com>
2021-01-27 19:55:04 +0000

    increase partition size

--- packer/target/icap-server/install-wizard.sh
+++ packer/target/icap-server/install-wizard.sh
@@ -42,3 +42,22 @@ sudo systemctl daemon-reload
 
 # enable initconfig for the next reboot
 sudo systemctl enable initconfig
+
+# increase partition size to maximum disk size
+sudo tee -a /etc/init.d/update_partition <<EOF
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:             update_partition
+# Required-Start:       $local_fs $remote_fs $network $syslog $named
+# Required-Stop:        $local_fs $remote_fs $network $syslog $named
+# Default-Start:        2 3 4 5
+# Default-Stop:         
+# Short-Description:    updates partition 
+# Description:          size to maximum disk size
+### END INIT INFO
+growpart /dev/sda 1
+resize2fs /dev/sda1
+EOF
+sudo chmod +x /etc/init.d/update_partition
+sudo update-rc.d update_partition defaults
